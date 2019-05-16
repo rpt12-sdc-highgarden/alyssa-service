c5764c6f9a717750315577321603f8efce5e1608
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-05-15 22:18:46 -0700

    Add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,21 @@ module.exports = {
     'commonjs': true,
     'node': true,
     'jest': true
+  },
+  rules: {
+    'no-console': 'off',
+    'comma-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'no-undef': 'off',
+    'import/extensions': 'always',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/prop-types': 'off',
+    'no-underscore-dangle': 'off',
+    'array-callback-return': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-destructuring': 'off',
+    'object-shorthand': 'off',
+    'radix': 'off'
   }
 };
