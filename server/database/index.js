818248c666e455e9f7c9d3e9bf5eb0631fa3065f
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-05-18 15:48:24 -0700

    Add function to create a new book

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const fake = require('faker');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', { useNewUrlParser: true });
 
 const bookSchema = mongoose.Schema({
   id: {
@@ -129,9 +129,92 @@ const retrieve = (id, callback) => {
   });
 };
 
+//This function is not being used anywhere. This is just to create a new book example for Postman testing.
+const createBook = () => {
+  var newBook = {}
+  // newBook.id = i;
+  newBook.title = fake.random.words(3);
+  newBook.author = fake.name.findName();
+  newBook.description = fake.lorem.paragraphs();
+  newBook.ratings = {
+    five: fake.random.number(),
+    four: fake.random.number(),
+    three: fake.random.number(),
+    two: fake.random.number(),
+    one: fake.random.number(),
+  };
+  newBook.reviews = fake.random.number();
+  newBook.links = {
+    kindle: fake.internet.url(),
+    amazon: fake.internet.url(),
+    stores: {
+      audible: fake.internet.url(),
+      barnesAndNoble: fake.internet.url(),
+      walmart: fake.internet.url(),
+      apple: fake.internet.url(),
+      google: fake.internet.url(),
+      abebooks: fake.internet.url(),
+      bookDesository: fake.internet.url(),
+      indigo: fake.internet.url(),
+      alibris: fake.internet.url(),
+      betterWorldBooks: fake.internet.url(),
+      indieBound: fake.internet.url(),
+    },
+  };
+  newBook.type = fake.random.word();
+  newBook.pages = fake.random.number({ max: 3000 });
+  newBook.publishDate = fake.date.past();
+  newBook.publisher = fake.company.companyName();
+  newBook.metadata = {
+    originalTitle: newBook.title,
+    isbn: fake.random.number(),
+    isbn13: fake.random.number(),
+    language: 'English',
+  };
+  newBook.image = 'http://lorempixel.com/480/640/abstract/';
+
+  return newBook;
+}
+//JSON example of a new book
+// {
+// 	"id": 200,
+// 	"title": "FTP Awesome Fresh Salad AGP",
+//   "author": "Mr. Burdette Barton",
+//   "description":
+//    "Quo voluptate voluptate. Ipsam omnis dolorem ut sequi velit blanditiis voluptatem. A est sint. Et neque impedit suscipit et dolor. Modi quas non asperiores tenetur. Odio quam quia fuga culpa.\n \rAut sit reprehenderit. Sed dignissimos consequatur hic error. Molestias aperiam ut praesentium dolor ex.\n \rPraesentium minus occaecati dolores. Quaerat omnis occaecati tenetur et voluptates. Amet quia minima molestiae provident quaerat. Fugit ullam sed earum vel quod aut nulla. Officiis sed quo enim animi qui necessitatibus.",
+//   "ratings":
+//    { "five": 52432, "four": 10146, "three": 7822, "two": 5583, "one": 65619 },
+//   "reviews": 23220,
+//   "links":
+//    { "kindle": "https://carolanne.net",
+//      "amazon": "https://gia.org",
+//      "stores":
+//       { "audible": "http://henderson.org",
+//         "barnesAndNoble": "https://jacinto.com",
+//         "walmart": "https://river.com",
+//         "apple": "http://estelle.org",
+//         "google": "https://jaylon.name",
+//         "abebooks": "http://elmore.info",
+//         "bookDesository": "http://omer.name",
+//         "indigo": "https://imelda.org",
+//         "alibris": "https://emily.name",
+//         "betterWorldBooks": "https://alaina.name",
+//         "indieBound": "http://lucienne.name" } },
+//   "type": "Developer",
+//   "pages": 1640,
+//   "publishDate": 2019-01-09T00:50:14.374Z,
+//   "publisher": "Morissette, Cummerata and Larkin",
+//   "metadata":
+//    { "originalTitle": "FTP Awesome Fresh Salad AGP",
+//      "isbn": 96556,
+//      "isbn13": 50300,
+//      "language": "English" },
+//   "image": "http://lorempixel.com/480/640/abstract/" }
+
 module.exports = {
   bookSchema,
   seed,
   Book,
   retrieve,
+  createBook
 };
