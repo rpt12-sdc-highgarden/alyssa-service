69fba8094ee8d6ba61c6f1f79572eb356cae5047
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-06-06 11:21:09 -0700

    Add retrieve function

--- server/database/mySQLIndex.js
+++ server/database/mySQLIndex.js
@@ -3,12 +3,22 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'books'
-});
-
-connection.connect( (err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL!');
+  database: 'goodreads'
 });
 
 //FILL IN IF YOU WILL PROCEED WITH MYSQL; NEED RETRIEVE FUNCTIONALITY
+
+const retrieve = (id, callback) => {
+  connection.connect((err) => {
+    if (err) throw err;
+    connection.query(`SELECT * FROM books where id = ${id}`, (err, result) => {
+      if (err) throw err;
+      console.log(result[0]);
+      callback(result[0]);
+    });
+  });
+}
+
+module.exports = {
+  retrieve
+}
