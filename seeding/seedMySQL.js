fc3442686b004ed5d68e86b4c960bf94401173bb
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-06-20 17:31:46 -0700

    Update seeding script for EC2

--- seeding/seedMySQL.js
+++ seeding/seedMySQL.js
@@ -20,43 +20,42 @@ const getRandomNumber = (max) => {
 const createBooks = () => {
   const newBooks = [];
   for (let i = 0; i < 10000; i += 1) {
-    let book = {};
-    book.title = fake.random.words(3);
-    book.author = fake.name.findName();
-    book.description = fake.lorem.paragraphs();
-    book.oneStar = fake.random.number();
-    book.twoStar = fake.random.number();
-    book.threeStar = fake.random.number();
-    book.fourStar = fake.random.number();
-    book.fiveStar = fake.random.number();
-    book.reviews = fake.random.number();
-    book.kindleLink = fake.internet.url();
-    book.amazonLink = fake.internet.url();
-    book.worldcatLink = fake.internet.url();
-    //store links
-    book.audibleLink = fake.internet.url();
-    book.barnesAndNobleLink = fake.internet.url();
-    book.walmartLink = fake.internet.url();
-    book.appleLink = fake.internet.url();
-    book.googleLink = fake.internet.url();
-    book.abebooksLink = fake.internet.url();
-    book.bookDepositoryLink = fake.internet.url();
-    book.indigoLink = fake.internet.url();
-    book.alibrisLink = fake.internet.url();
-    book.betterWorldBooksLink = fake.internet.url();
-    book.indieBoundLink = fake.internet.url();
+    let bookTitle = fake.random.words(3)
+    let book = {
+      title: bookTitle,
+      author: fake.name.findName(),
+      description: fake.lorem.paragraphs(),
+      oneStar: fake.random.number(),
+      twoStar: fake.random.number(),
+      threeStar: fake.random.number(),
+      fourStar: fake.random.number(),
+      fiveStar: fake.random.number(),
 
-    book.type = fake.random.word();
-    book.pages = fake.random.number({ max: 3000 });
-    book.publishDate = fake.date.past();
-    book.publisher = fake.company.companyName();
-    //metadata
-    book.originalTitle = book.title;
-    book.ISBN = fake.random.number();
-    book.ISBN13 = fake.random.number();
-    book.language = 'English';
+      //store links
+      audibleLink: fake.internet.url(),
+      barnesAndNobleLink: fake.internet.url(),
+      walmartLink: fake.internet.url(),
+      appleLink: fake.internet.url(),
+      googleLink: fake.internet.url(),
+      abebooksLink: fake.internet.url(),
+      bookDepositoryLink: fake.internet.url(),
+      indigoLink: fake.internet.url(),
+      alibrisLink: fake.internet.url(),
+      betterWorldBooksLink: fake.internet.url(),
+      indieBoundLink: fake.internet.url(),
 
-    book.imageURL = `https://s3-us-west-2.amazonaws.com/sdc-goodreads/book-images/${getRandomNumber(201)}.jpg`;
+      type: fake.random.word(),
+      pages: fake.random.number({ max: 3000 }),
+      publishDate: fake.date.past(),
+      publisher: fake.company.companyName(),
+      //metadata
+      originalTitle: bookTitle,
+      ISBN: fake.random.number(),
+      ISBN13: fake.random.number(),
+      language: 'English',
+
+      imageURL: `https://s3-us-west-2.amazonaws.com/sdc-goodreads/book-images/${getRandomNumber(201)}.jpg`
+    };
 
     if (i % 2 === 0) {
       book.series_name = fake.random.words(2);
@@ -73,23 +72,22 @@ const createBooks = () => {
 
 const seed = async () => {
   var chunkSize = 1000;
-  console.log(`Seeding started on ${Date()}`);
-  for (var i = 0; i < 1000; i++) {
-    var newBooks = createBooks();
-    //WILL LEAVE THIS FOR NOW, testing for EC2 deployment
-    // await knex.batchInsert('books', newBooks, chunkSize)
-    // .catch((error) => { throw error });
-    try {
+
+  try {
+    console.log(`Seeding started on ${Date()}`);
+    for (var i = 0; i < 1000; i++) {
+      var newBooks = createBooks();
       await knex.batchInsert('books', newBooks, chunkSize)
-    } catch (error) {
-      throw error;
+
+      if (i % 100 === 0) {
+        console.log(`Seeded ${i/100}M records`);
+      }
     }
-    if (i % 100 === 0) {
-      console.log(`Seeded ${i/100}M records`);
-    }
+    console.log(`Seeded ${i/100}M records. Completed on ${Date()}`);
+    process.exit(0);
+  } catch (error) {
+    console.error('CATCH ERROR', error);
   }
-  console.log(`Seeded ${i/100}M records. Completed on ${Date()}`);
-  process.exit(0);
 }
 
 seed();
